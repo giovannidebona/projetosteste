8aff202d2a23c97da12949676b581a1737261b3d
Giovanni Christian Debona <giovanni.debona@zenatur.com.br>
2021-11-22 15:29:20 -0300

    first commit

---
+++ frmDecompositor.Designer.cs
@@ -0,0 +1,130 @@
+﻿
+namespace ProvaFramework1
+{
+    partial class frmDecompositor
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtNumero = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnVerificar = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtURLAPI = new System.Windows.Forms.TextBox();
+            this.txtResultado = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(104, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Insira um número:";
+            // 
+            // txtNumero
+            // 
+            this.txtNumero.Location = new System.Drawing.Point(122, 38);
+            this.txtNumero.Name = "txtNumero";
+            this.txtNumero.Size = new System.Drawing.Size(100, 23);
+            this.txtNumero.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 76);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(62, 15);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Resultado:";
+            // 
+            // btnVerificar
+            // 
+            this.btnVerificar.Location = new System.Drawing.Point(237, 38);
+            this.btnVerificar.Name = "btnVerificar";
+            this.btnVerificar.Size = new System.Drawing.Size(75, 23);
+            this.btnVerificar.TabIndex = 3;
+            this.btnVerificar.Text = "Verificar";
+            this.btnVerificar.UseVisualStyleBackColor = true;
+            this.btnVerificar.Click += new System.EventHandler(this.btnVerificar_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(68, 15);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "URL da API:";
+            // 
+            // txtURLAPI
+            // 
+            this.txtURLAPI.Location = new System.Drawing.Point(122, 6);
+            this.txtURLAPI.Name = "txtURLAPI";
+            this.txtURLAPI.Size = new System.Drawing.Size(315, 23);
+            this.txtURLAPI.TabIndex = 6;
+            // 
+            // txtResultado
+            // 
+            this.txtResultado.Location = new System.Drawing.Point(12, 94);
+            this.txtResultado.Multiline = true;
+            this.txtResultado.Name = "txtResultado";
+            this.txtResultado.Size = new System.Drawing.Size(425, 62);
+            this.txtResultado.TabIndex = 7;
+            // 
+            // frmDecompositor
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(449, 182);
+            this.Controls.Add(this.txtResultado);
+            this.Controls.Add(this.txtURLAPI);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnVerificar);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtNumero);
+            this.Controls.Add(this.label1);
+            this.Name = "frmDecompositor";
+            this.Text = "Decompositor de números";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtNumero;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnVerificar;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtURLAPI;
+        private System.Windows.Forms.TextBox txtResultado;
+    }
+}
+
---
+++ frmDecompositor.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+
+namespace ProvaFramework1
+{
+    public partial class frmDecompositor : Form
+    {
+        public frmDecompositor()
+        {
+            InitializeComponent();
+        }
+
+        private void btnVerificar_Click(object sender, EventArgs e)
+        {
+            GetNumeros();
+        }
+
+        private async void GetNumeros()
+        {
+            string controller = "numero";
+            string medoto = "GetListDivisores";
+            var numerico = int.TryParse(txtNumero.Text, out int numero);
+            string URI = txtURLAPI.Text + "/v1/" + controller + "/" + medoto + "/" + numero;
+
+            if (numerico && numero > 0)
+            {
+                using (var client = new HttpClient())
+                {
+                    using (var response = await client.GetAsync(URI))
+                    {
+                        if (response.IsSuccessStatusCode)
+                        {
+                            var retornoJSON = await response.Content.ReadAsStringAsync();
+                            txtResultado.Text = retornoJSON;
+                        }
+                        else
+                        {
+                            MessageBox.Show("Não foi possível obter os dados : " + response.StatusCode);
+                        }
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("Informe um número inteiro maior do que 0.");
+            }
+        }
+    }
+}
